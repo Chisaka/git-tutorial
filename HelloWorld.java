724f337d83528e694720d54f701451cd029c6139
Chisaka <jjs-0004@outlook.jp>
2014-05-09 16:58:42 +0900

    first java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+		public static void main(String[] args) {
+				System.out.print("‚±‚ñ‚É‚¿‚Í¢ŠE");
+				System.out.println("¢ŠE");
+				System.out.println("vi‚Å•ÒW‚µ‚Ü‚µ‚½");
+		}
+}
